059510ecdbc00050809e2eb356acaf235bafa32b
Janamajaya Mall <janmajaya.choithram@gmail.com>
2018-10-17 03:28:11 +0800

    Added contact conditions

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ const server = express();
 //Define Intent Requests
 
 //Intents for Directions
-const requestDirectionsIdConfirmed = 'Request Directions - ID Confirmed'; // reply enter again
+const requestDirectionsIDConfirmed = 'Request Directions - ID Confirmed'; // reply enter again
 const requestDirectionsIDNotConfirmedEnteredAgain = 'Request Directions - ID Not Confirmed - Entered Again';  // reply enter again
-const requestDirectionsIdConfirmedNotFound = 'Request Directions - ID confirmed - Not Found'; // reply call helpline
+const requestDirectionsIDConfirmedNotFoundLast = 'Request Directions - ID Confirmed - Not Found - Last'; // reply call helpline
+const requestDirectionsIDNotConfirmedEnteredAgainNotFoundLast = 'Request Directions - ID Not Confirmed - Entered Again - Not Found - Last'; // reply call helpline
+
+//Intents for Contact
+const requestContactIDConfirmed = 'Request Contact - ID Confirmed'; // reply enter again
+const requestContactIDNotConfirmedEnteredAgain = 'Request Contact - ID Not Confirmed - Entered Again';  // reply enter again
+const requestContactIDConfirmedNotFoundLast = 'Request Contact - ID Confirmed - Not Found - Last'; // reply call helpline
+const requestContactIDNotConfirmedEnteredAgainNotFoundLast = 'Request Contact - ID Not Confirmed - Entered Again - Not Found - Last'; // reply call helpline
+
 
 //Intents for Contact-info
 const requestContactInfo = 'Request Contact-info'
@@ -30,26 +38,33 @@ server.post('/get-info', (req, res)=>{
     const intent = req.body.queryResult &&  req.body.queryResult.intent && req.body.queryResult.intent.displayName ? 
                                                                             req.body.queryResult.intent.displayName : null;
     
-
+    
     //Response according to intent
-    if (intent === requestDirectionsIdConfirmed || intent === requestDirectionsIDNotConfirmedEnteredAgain || intent === requestDirectionsIdConfirmedNotFound){
 
-            const bookingID = null;
+    // Intent Request Directions
+    if (intent === requestDirectionsIDConfirmed || intent === requestDirectionsIDNotConfirmedEnteredAgain || intent === requestDirectionsIDConfirmedNotFoundLast || intent === requestDirectionsIDNotConfirmedEnteredAgainNotFoundLast){
 
-            if (intent === requestDirectionsIdConfirmed){
+        let bookingID = null;
+
+            if (intent === requestDirectionsIDConfirmed){
+                
                 bookingID = req.body.queryResult &&  req.body.queryResult.outputContexts[0].lifespanCount === 2  ? 
                                                                         req.body.queryResult.outputContexts[1].parameters.bookingID : null;
             } else if (intent === requestDirectionsIDNotConfirmedEnteredAgain){
                 bookingID = req.body.queryResult &&  req.body.queryResult.parameters && req.body.queryResult.parameters.bookingID ? 
                                                                                     req.body.queryResult.parameters.bookingID : null;
-            } else if (intent === requestDirectionsIdConfirmedNotFound) {
+            } else if (intent === requestDirectionsIDConfirmedNotFoundLast) {
                 bookingID = req.body.queryResult &&  req.body.queryResult.outputContexts  ? 
                                                                         req.body.queryResult.outputContexts[0].parameters.bookingID : null;
+            } else if (intent === requestDirectionsIDNotConfirmedEnteredAgainNotFoundLast){
+                bookingID = req.body.queryResult &&  req.body.queryResult.parameters && req.body.queryResult.parameters.bookingID ? 
+                                                                                    req.body.queryResult.parameters.bookingID : null;
             }
 
-            const bookingID = req.body.queryResult &&  req.body.queryResult.parameters && req.body.queryResult.parameters.bookingID ? 
-                                                                                            req.body.queryResult.parameters.bookingID : null;
+            
+
             const reqURL = encodeURI(`http://internal.bukitvista.com/tools/api/integromatcancelation/${bookingID}`);
+
 
             http.get(reqURL, (responseFromAPI)=>{
                 let completeResponse = '';
@@ -61,18 +76,30 @@ server.post('/get-info', (req, res)=>{
 
                 let customerDetails = '';
 
+
+
                 // Try and catch block if the value is JSON; if JSON then pass on, other throw and error for cathc block
                 try{
                 JSON.parse(completeResponse);
 
                 } catch(e){
-                    return res.json({
-                    fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
-                    source: 'get-directions'
-                    });
+
+                    if(intent === requestDirectionsIDConfirmedNotFoundLast || intent == requestDirectionsIDNotConfirmedEnteredAgainNotFoundLast){
+                        return res.json({
+                            fulfillmentText: 'Please call helpline!',
+                            source: 'get-directions'
+                            });
+                    }else {
+                        return res.json({
+                            fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
+                            source: 'get-directions'
+                            });
+                        }  
                 }
                 // End of try catch;    The value recieved is JSON.
 
+
+                // Reading the returned json
                 customerDetails = JSON.parse(completeResponse);
                 let sscbResponse = `Hi, ${customerDetails.booking_guest_name}. ${customerDetails.property_direction}.`;
                 return res.json({
@@ -80,50 +107,99 @@ server.post('/get-info', (req, res)=>{
                     source: 'get-directions'});
                     })
                 }, (error)=>{
-                    return res.json({
-                    fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
-                    source: 'get-directions'
-                    });
+                    //Checking if it is not the ending intent -- Ending intent should present helpline number
+                    if(intent === requestDirectionsIdConfirmedNotFoundLast || intent == requestDirectionsIDNotConfirmedEnteredAgainNotFoundLast){
+                        return res.json({
+                            fulfillmentText: 'Please call helpline!',
+                            source: 'get-directions'
+                            });
+                    }else {
+                        return res.json({
+                            fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
+                            source: 'get-directions'
+                            });
+                        }  
                 });
     }
-    else if (intent === requestContactInfo){
-            const bookingID = req.body.queryResult &&  req.body.queryResult.parameters && req.body.queryResult.parameters.bookingID ? 
-                                                                                            req.body.queryResult.parameters.bookingID : null;
-            const reqURL = encodeURI(`http://internal.bukitvista.com/tools/api/integromatcancelation/${bookingID}`);
 
+
+
+
+    // Intent request contact info
+    else if (intent === requestContactIDConfirmed || intent === requestContactIDNotConfirmedEnteredAgain || intent === requestContactIDConfirmedNotFoundLast || intent === requestContactIDNotConfirmedEnteredAgainNotFoundLast){
+
+        let bookingID = null;
+    
+            if (intent === requestContactIDConfirmed){
+                
+                bookingID = req.body.queryResult &&  req.body.queryResult.outputContexts[0].lifespanCount === 2  ? 
+                                                                        req.body.queryResult.outputContexts[1].parameters.bookingID : null;
+            } else if (intent === requestContactIDNotConfirmedEnteredAgain){
+                bookingID = req.body.queryResult &&  req.body.queryResult.parameters && req.body.queryResult.parameters.bookingID ? 
+                                                                                    req.body.queryResult.parameters.bookingID : null;
+            } else if (intent === requestContactIDConfirmedNotFoundLast) {
+                bookingID = req.body.queryResult &&  req.body.queryResult.outputContexts  ? 
+                                                                        req.body.queryResult.outputContexts[0].parameters.bookingID : null;
+            }
+    
+            
+    
+            const reqURL = encodeURI(`http://internal.bukitvista.com/tools/api/integromatcancelation/${bookingID}`);
+    
+    
             http.get(reqURL, (responseFromAPI)=>{
                 let completeResponse = '';
                         responseFromAPI.on('data', (chunk)=>{
                         completeResponse += chunk;
                     });
-
+    
                 responseFromAPI.on('end', ()=>{
-
+    
                 let customerDetails = '';
-
+    
+    
+    
                 // Try and catch block if the value is JSON; if JSON then pass on, other throw and error for cathc block
                 try{
                 JSON.parse(completeResponse);
-
+    
                 } catch(e){
-                    return res.json({
-                    fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
-                    source: 'get-directions'
-                    });
+    
+                    if(intent === requestContactIDConfirmedNotFoundLast || intent == requestContactIDNotConfirmedEnteredAgainNotFoundLast){
+                        return res.json({
+                            fulfillmentText: 'Please call helpline!',
+                            source: 'get-contact'
+                            });
+                    }else {
+                        return res.json({
+                            fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
+                            source: 'get-contact'
+                            });
+                        }  
                 }
                 // End of try catch;    The value recieved is JSON.
-
+    
+    
+                // Reading the returned json
                 customerDetails = JSON.parse(completeResponse);
-                let sscbResponse = `Hi, ${customerDetails.booking_guest_name}. Your booking reference is ${customerDetails.booking_guest_phone}.`;
+                let sscbResponse = `Hi, ${customerDetails.booking_guest_name}. ${customerDetails.booking_guest_phone}.`;
                 return res.json({
                     fulfillmentText: sscbResponse,
-                    source: 'get-directions'});
+                    source: 'get-contact'});
                     })
                 }, (error)=>{
-                    return res.json({
-                    fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
-                    source: 'get-directions'
-                    });
+                    //Checking if it is not the ending intent -- Ending intent should present helpline number
+                    if(intent === requestDirectionsIdConfirmedNotFoundLast || intent == requestContactIDNotConfirmedEnteredAgainNotFoundLast){
+                        return res.json({
+                            fulfillmentText: 'Please call helpline!',
+                            source: 'get-contact'
+                            });
+                    }else {
+                        return res.json({
+                            fulfillmentText: 'Sorry we could not find you in our guest database, try again?',
+                            source: 'get-contact'
+                            });
+                        }  
                 });
     }
 
